90ccaf5cb19dd4bbea7aca842a88210b43fdf64a
Leon <yanglyu.leon.7@gmail.com>
2017-09-30 14:54:13 -0700

    fea: HotDogDataService has been built

---
+++ RaysHotDog.Core/Service/HotDogDataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using RaysHotDog.Core.Repository;
+using RaysHotDog.Core.Model;
+
+namespace RaysHotDog.Core.Service
+{
+    public class HotDogDataService
+    {
+        private static HotDogRepository hotDogRepository = new HotDogRepository();
+
+        public HotDogDataService()
+        {
+        }
+
+        public List<HotDog> GetAllHotDogs(){
+            return hotDogRepository.GetAllHotDogs();
+        }
+
+        public List<HotDogGroup> GetGroupedHotDogs(){
+            return hotDogRepository.GetGroupedHotDogs();
+        }
+
+        public List<HotDog> GetHotDogsForGroup(int hotDogGroupId){
+            return hotDogRepository.GetHotDogsForGroup(hotDogGroupId);
+        }
+    }
+}
